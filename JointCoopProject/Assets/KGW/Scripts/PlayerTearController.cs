a7187d83f84405daf6722bd54c358dcf734e8843
KimGiWoon <rldns20704@naver.com>
2025-06-21 17:40:55 +0900

    [Feat] Player Move Dash Implement
    플레이어의 대쉬 동작 구현 (쉬프트키를 눌러 동작하며 플레이어가 이동 시 대쉬 가능)

--- JointCoopProject/Assets/KGW/Scripts/PlayerTearController.cs
+++ JointCoopProject/Assets/KGW/Scripts/PlayerTearController.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IsaacTearController : MonoBehaviour
+public class PlayerTearController : MonoBehaviour
 {
     [SerializeField] float _tearLifeTime = 3f;
-    [SerializeField][Range(1, 3)] int _tearDamage = 1;
+    //[SerializeField][Range(1, 3)] int _tearDamage = 1;
 
     private void Start()
     {
