7140bc0217e700c5d73fe2f0a5e7f084f38465b6
Huge-Chamchi <abaebae96@gmail.com>
2025-06-23 01:06:30 +0900

    [chore]
    [chore]

--- JointCoopProject/Assets/USW/ProtoType/PlayerSkill/PlayerSkillManager.cs
+++ JointCoopProject/Assets/USW/ProtoType/PlayerSkill/PlayerSkillManager.cs
@@ -8,18 +8,28 @@ using UnityEngine;
 /// </summary>
 public class PlayerSkillManager : MonoBehaviour
 {
+   // 플레이어가 현재 보유 중인 스킬 목록
    private List<SkillDataSO> ownedSkills = new();
 
+   
+   /// <summary>
+   /// 자동으로 발동되는 스킬 정보를 저장하는 클래스
+   /// </summary>
    [System.Serializable]
    public class AutoSkill
    {
-      public SkillDataSO skill;
-      public float cooldown = 5f;
-      public float timer;
+      public SkillDataSO skill;     // 자동 발동할 스킬 데이터
+      public float cooldown = 5f;   // 쿨다운 간격 (초단위입니다!)
+      public float timer;           // 현재 남은 시간 (Update에서 감소합니다)
    }
    
+   // 자동 발동 스킬들의 리스트
    public List<AutoSkill> autoskills = new();
 
+   
+   /// <summary>
+   /// 매 프레임마다 자동 스킬 타이머를 감소시키고 , 조건을 만족하면 스킬을 발동.
+   /// </summary>
    private void Update()
    {
       foreach (var skill in autoskills)
@@ -33,7 +43,10 @@ public class PlayerSkillManager : MonoBehaviour
       }
    }
 
-
+   /// <summary>
+   /// 스킬 이름으로 스킬을 찾아서 발동함.
+   /// </summary>
+   /// <param name="skillName">발동하려는 스킬 이름</param>
    public void AddSkill(SkillDataSO newSkill)
    {
       if (!ownedSkills.Contains(newSkill))
@@ -41,9 +54,14 @@ public class PlayerSkillManager : MonoBehaviour
          ownedSkills.Add(newSkill);
       }
    }
-
+   
+   /// <summary>
+   /// 스킬 이름으로 스킬을 찾아서 발동합니다.
+   /// </summary>
+   /// <param name="skillName">발동하고자 하는 스킬 이름</param>
    public void UseSkill(string skillName)
    {
+      // 보유 스킬 목록에서 이름이 일치하는 스킬을 찾음.
       var skill = ownedSkills.Find(s=>s.skillName == skillName);
 
       if (skill != null)
--- JointCoopProject/Assets/USW/ProtoType/PlayerSkill/SkillDictionarySO.cs
+++ JointCoopProject/Assets/USW/ProtoType/PlayerSkill/SkillDictionarySO.cs
@@ -4,21 +4,35 @@ using System.Linq;
 using UnityEngine;
 
 
+
+
 /// <summary>
 /// 모든 스킬이 담겨져있는 Dictionary 라고 생각하시면 됩니다 ( ScriptableObject )
 /// </summary>
 [CreateAssetMenu(menuName = "PlayerSkill/SkillDictionarySO")]
 public class SkillDictionarySO : ScriptableObject
 {
+        
     public List<SkillDataSO> skillList;
-
+    
+    // 조회를 위해 사용하는 딕셔너리 (key - skill name, value - SkillDataSO 
     private Dictionary<string, SkillDataSO> _skilldictionary;
 
+    
+    /// <summary>
+    /// skillList를 바탕으로 딕셔너리 초기화
+    /// </summary>
     public void Init()
     {
         _skilldictionary = skillList.ToDictionary(s=>s.skillName, s=>s);
     }
 
+    
+    /// <summary>
+    /// 스킬 이름으로 SkillDataSO 조회함.
+    /// </summary>
+    /// <param name="찾고자 하는 스킬의 이름"></param>
+    /// <returns>해당 이름의 SkillDataSO 객체</returns>
     public SkillDataSO GetSkill(string name)
     {
         if(_skilldictionary ==null) Init();
