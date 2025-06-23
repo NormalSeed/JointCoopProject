f9ca9216f66930b6ddcaabba023541856415660d
KimGiWoon <rldns20704@naver.com>
2025-06-23 22:44:20 +0900

    [Feat] Add Player Hit, Dash Animation
    플레이어의 대쉬와 피격 시 애니메이션 추가 및 플레이어 무브 애니메이션 수정

--- JointCoopProject/Assets/KGW/Scripts/MonsterTest.cs
+++ JointCoopProject/Assets/KGW/Scripts/MonsterTest.cs
@@ -8,4 +8,16 @@ public class MonsterTest : MonoBehaviour, IDamagable
     {
         Debug.Log($"몬스터가 {damage}를 받았습니다.");
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+            if(damagable != null)
+            {
+                damagable.TakeDamage(1);
+            }
+        }
+    }
 }
