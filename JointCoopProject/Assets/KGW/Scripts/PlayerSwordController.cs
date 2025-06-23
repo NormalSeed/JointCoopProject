ee3696c11b8a1f24c55e69f8557d8bbed165cf85
KimGiWoon <rldns20704@naver.com>
2025-06-23 16:27:30 +0900

    [Feat] Add Player Movement Animation
    플레이어의 Idle, Walk 애니메이션 추가

--- JointCoopProject/Assets/KGW/Scripts/PlayerSwordController.cs
+++ JointCoopProject/Assets/KGW/Scripts/PlayerSwordController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using TreeEditor;
 using UnityEngine;
 
@@ -7,38 +8,34 @@ public class PlayerSwordController : MonoBehaviour
 {
     Transform _playerPos;
 
-    float _wieldDuration = 0.3f;
-    float _timer = 0;
     int _swordDamage = 1;
-    Vector2 _wieldDirection;
-    float _wieldAngle = 120f;
-    float _rotationSpeed = 420f;
-    float _wieldRadius = 0.5f;
-    float _currentAngle = 0f;
-    float _startAngle = 0f;
-    float _wieldSpeed;
+    Vector2 _wieldDirection;        // 휘두르는 방향 벡터
+    float _wieldAngle = 90f;       // 무기의 휘두름 반경
+    float _rotationSpeed = 420f;    // 회전 속도 (값이 높을 수록 빨리 회전한다)
+    float _wieldRadius = 0.3f;      // 무기 회전 반경
+    float _currentAngle = 0;       // 현재 각도
+    float _startAngle = 0;         // 시작 각도
+    float _wieldSpeed;              // 플레이어의 공격 속도
 
     private void Update()
     {
-        if (_playerPos == null)
+        if (_playerPos == null) // 플레이어가 무기를 소지하지 않으면 디스트로이
         {
             Destroy(gameObject);
             return;
         }
 
-        //_timer -= Time.deltaTime;
-
-        float angle = _rotationSpeed * _wieldSpeed * Time.deltaTime;
-        _currentAngle += angle;
+        float angle = _rotationSpeed * _wieldSpeed * Time.deltaTime;    // 회전스피드가 플레이어의 공격스피드에 영향을 받아 빨리 휘두름
+        _currentAngle -= angle;
    
         // 현재 각도로 위치를 플레이어의 중심을 기준으로 재계산
         float radian = (_currentAngle + _startAngle) * Mathf.Deg2Rad;
-        Vector3 posOffset = new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0f) * _wieldRadius;
+        Vector3 posOffset = new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0f) * _wieldRadius;   // 현재의 각도와 시작각도를 더한 값에 회전 반경을 곱해서 위치 옵셋 세팅
         transform.position = _playerPos.position + posOffset;
 
-        transform.rotation = Quaternion.Euler(0, 0, _currentAngle + _startAngle - 80f);
+        transform.rotation = Quaternion.Euler(0, 0, _currentAngle + _startAngle + 90f);
 
-        if (_currentAngle >= _wieldAngle)
+        if (_currentAngle <= -_wieldAngle)
         {
             Destroy(gameObject);
         }
@@ -48,9 +45,11 @@ public class PlayerSwordController : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            if(collision.GetComponent<IDamagable>() != null)
+            Debug.Log("적을 공격했습니다.");
+            IDamagable damagable = collision.GetComponent<IDamagable>();
+            if (damagable != null)
             {
-                // 대상이 공격을 받을 수 있으므로 데미지를 준다.
+                damagable.TakeDamage(_swordDamage);
             }
         }
     }
@@ -59,7 +58,6 @@ public class PlayerSwordController : MonoBehaviour
     {
         _playerPos = player;
         _wieldDirection = direction.normalized;
-        _timer = _wieldDuration;
         _wieldSpeed = attackSpeed;
         _currentAngle = 0f;
 
@@ -69,7 +67,7 @@ public class PlayerSwordController : MonoBehaviour
         Vector3 firstOffset = new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0f) * _wieldRadius;
         transform.position = _playerPos.position + firstOffset;
 
-        transform.rotation = Quaternion.Euler(0, 0, _currentAngle + _startAngle - 80f);
+        transform.rotation = Quaternion.Euler(0, 0, _currentAngle + _startAngle + 90f);
 
     }
 }
