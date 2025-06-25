c73feb89f1d6761ad18d5de3a4616f82c5e92ae4
Young-EunC <mango3254@gmail.com>
2025-06-25 16:22:01 +0900

    [Chore] Create Inventory Script

---
+++ JointCoopProject/Assets/CYE/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Active Item - Slot 1
+    // Trait Item - Slot 5 -> 플레이어에 귀속될 수 있음
+    // Passive Item - Slot 1/List
+    // Enhance Item - List
+    // Expendable(Coin, Bomb) -> 플레이어에 귀속될 수 있음
+
+    
+}
