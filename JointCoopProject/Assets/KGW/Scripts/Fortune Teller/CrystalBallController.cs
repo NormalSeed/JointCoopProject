61ed844424ab7ab8a1c35431424889f90131e984
KimGiWoon <rldns20704@naver.com>
2025-06-30 12:44:37 +0900

    [Feat] Add Buff Effect
    축복, 저주 ,꽝 버프의 1,2,3,4,5단계 버프 획득 이펙트 추가

--- JointCoopProject/Assets/KGW/Scripts/Fortune Teller/CrystalBallController.cs
+++ JointCoopProject/Assets/KGW/Scripts/Fortune Teller/CrystalBallController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class CrystalBallController : MonoBehaviour
 {
+    [SerializeField] Animator _effectAni;
     bool _isContact = false;
+
+    readonly int Bless_Common_Effect = Animator.StringToHash("BlessCommonEffect");
+    readonly int Bless_Rare_Effect = Animator.StringToHash("BlessRareEffect");
+    readonly int Bless_Legend_Effect = Animator.StringToHash("BlessLegendEffect");
+    readonly int Curs_Common_Effect = Animator.StringToHash("CursCommonEffect");
+    readonly int Curs_Rare_Effect = Animator.StringToHash("CursRareEffect");
+    readonly int Curs_Legend_Effect = Animator.StringToHash("CursLegendEffect");
+    readonly int Nothing_Effect = Animator.StringToHash("NothingEffect");
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -27,6 +36,43 @@ public class CrystalBallController : MonoBehaviour
             //_isContact = true;
 
             // ¡¢√ÀΩ√ ªÁøÓµÂøÕ ¿Ã∆Â∆Æ
+            // √‡∫π ¿Ã∆Â∆Æ
+            if (randomBuff._buffCategory == BuffCategory.Blessing)
+            {
+                if (randomBuff._buffLevel == 1 ||  randomBuff._buffLevel == 2)
+                {
+                    _effectAni.Play(Bless_Common_Effect);   // √‡∫π Level1,2 ¿Ã∆Â∆Æ
+                }
+                else if (randomBuff._buffLevel == 3 || randomBuff._buffLevel == 4)
+                {
+                    _effectAni.Play(Bless_Rare_Effect);     // √‡∫π Level3,4 ¿Ã∆Â∆Æ
+                }
+                else
+                {
+                    _effectAni.Play(Bless_Legend_Effect);   // √‡∫π Level5 ¿Ã∆Â∆Æ
+                }
+            }
+            // ¿˙¡÷ ¿Ã∆Â∆Æ
+            if (randomBuff._buffCategory == BuffCategory.Curs)
+            {
+                if (randomBuff._buffLevel == 1 || randomBuff._buffLevel == 2)
+                {
+                    _effectAni.Play(Curs_Common_Effect);    // ¿˙¡÷ Level1,2 ¿Ã∆Â∆Æ
+                }
+                else if (randomBuff._buffLevel == 3 || randomBuff._buffLevel == 4)
+                {
+                    _effectAni.Play(Curs_Rare_Effect);      // ¿˙¡÷ Level3,4 ¿Ã∆Â∆Æ
+                }
+                else
+                {
+                    _effectAni.Play(Curs_Legend_Effect);    // ¿˙¡÷ Level5 ¿Ã∆Â∆Æ
+                }
+            }
+            // ≤Œ ¿Ã∆Â∆Æ
+            if(randomBuff._buffCategory == BuffCategory.Nothing)
+            {
+                _effectAni.Play(Nothing_Effect);            // ≤Œ ¿Ã∆Â∆Æ
+            }
         }
     }
 }
