4d5d1b5a4bde0f144bf7931439bb87b88d33f550
Young-EunC <mango3254@gmail.com>
2025-07-02 13:58:03 +0900

    [Feat] Edit Item Structure
    1. IInstallable 인터페이스 추가(설치형 아이템을 위함)
    2. ItemSO에서 아이템 중복 가능 여부 제외(소모성 아이템은 해당 여부를 체크하는것이 무의미함)
    3. 미사용 스크립트 주석처리

--- JointCoopProject/Assets/CYE/Scripts/Items/S/Item.cs
+++ JointCoopProject/Assets/CYE/Scripts/Items/S/Item.cs
@@ -8,11 +8,7 @@ public abstract class Item : MonoBehaviour
 {
     [Header("Item Info")]
     [Tooltip("아이템 타입을 결정합니다.\n - Active: 액티브 \n - PassiveAttack: 패시브-공격 강화 \n - PassiveAuto: 패시브-상시 활성화 ")]
-    public ItemType _itemType;
-    [SerializeField]
-    [Tooltip("아이템 중복 획득 가능 여부를 결정합니다.")]
-    protected bool _canStackable;
-    
+    public ItemType _itemType;    
 
     [Header("Item Data")]
     public ItemDataSO _itemData;
@@ -26,5 +22,6 @@ public abstract class Item : MonoBehaviour
     {
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         _spriteRenderer.sprite = _itemData._itemIcon;
+        _spriteRenderer.color = Color.white;
     }
 }
