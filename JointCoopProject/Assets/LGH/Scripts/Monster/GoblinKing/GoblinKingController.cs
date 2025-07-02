8bd140577d5b0ce604558e41c70aa6272aa5a909
NormalSeed <dlrjsghk11@naver.com>
2025-07-02 11:20:00 +0900

    [Feat] Goblin King Prefab
    고블린 킹 프리팹 작성 중

---
+++ JointCoopProject/Assets/LGH/Scripts/Monster/GoblinKing/GoblinKingController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinKingController : MonsterBase
+{
+    protected override void Init()
+    {
+        base.Init();
+        _monsterID = 10352;
+    }
+
+
+}
