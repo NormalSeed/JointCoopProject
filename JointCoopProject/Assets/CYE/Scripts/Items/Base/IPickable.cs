36bb9b5435835f116c6a00b4a517cb615dd5cd7e
Young-EunC <mango3254@gmail.com>
2025-07-04 13:40:33 +0900

    [Chore] Create Item Prefabs
    개별 아이템 프리팹 생성

--- JointCoopProject/Assets/CYE/Scripts/Items/Base/IPickable.cs
+++ JointCoopProject/Assets/CYE/Scripts/Items/Base/IPickable.cs
@@ -11,14 +11,6 @@ public enum ItemType
 {
     Active, PassiveAttack, PassiveAuto, shop, Expend
 }
-public enum NonSkillItemType
-{ 
-    Enhance, Expend
-}
-public enum ChangeTarget
-{
-    CurHp, AttackPoint, AttackSpeed, MoveSpeed, Luck
-}
 public interface IInstallable
 {
     public void Install(Transform setUpPos);
--- JointCoopProject/Assets/CYE/Scripts/Items/Base/ShopItem.cs
+++ JointCoopProject/Assets/CYE/Scripts/Items/Base/ShopItem.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class ShopItem : Item, IPickable
 {
     [SerializeField]
-    [Tooltip("아이템 중복 획득 가능 여부를 결정합니다.")]
+    [Tooltip("아이템 중복 획득 가능 여부를 결정합니다. 체크하면 획득시 인벤토리에 표시됩니다.")]
     private bool isVisibleInInventory;
     public bool _isVisibleInInventory { get { return isVisibleInInventory; } }
+
+    [SerializeField]
+    [Tooltip("아이템 하단에 가격 표시 여부를 결정합니다. 체크하면 가격이 표시되지 않습니다.")]
+    private bool showPrice;
+    public bool _showPrice { get { return showPrice; } }
     
     #region // Unity Message Function
     void Awake()
