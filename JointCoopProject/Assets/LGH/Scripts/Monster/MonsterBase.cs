6249dd6034d47cc4c82c4a2e56b06ff55d431256
NormalSeed <dlrjsghk11@naver.com>
2025-07-05 17:47:13 +0900

    [Fix] Monster Detect Player
    Player라는 이름이 아닌 Player(Clone)이라는 이름을 찾아서 Player로 인식하도록 설정

--- JointCoopProject/Assets/LGH/Scripts/Monster/MonsterBase.cs
+++ JointCoopProject/Assets/LGH/Scripts/Monster/MonsterBase.cs
@@ -56,7 +56,7 @@ public abstract class MonsterBase : MonoBehaviour, IDamagable
         _model = GetComponent<MonsterModel>();
         _movement = GetComponent<MonsterMovement>();
         _view = GetComponent<MonsterView>();
-        _player = GameObject.Find("Player");
+        _player = GameObject.Find("Player(Clone)");
 
         roomMonsterManager = FindObjectOfType<RoomMonsterManager>();
         
